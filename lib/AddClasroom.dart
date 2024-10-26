f8d7ad5cda58afa8112914ee5571d12aca1f7c7c
KimSeoWon <ksw1912@gmail.com>
2024-10-26 21:43:32 +0900

    커밋

---
+++ lib/AddClasroom.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddClasroom extends StatefulWidget {
+  const AddClasroom({Key? key}) : super(key: key);
+
+  @override
+  _AddClasroomState createState() => _AddClasroomState();
+}
+
+class _AddClasroomState extends State<AddClasroom> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
